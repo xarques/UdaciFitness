a3a793f01b5ed51a22c1ca5b251d53ddea44ef18
Xavier Arques <xavierarques@yahoo.com>
2018-06-01 16:23:12 +0200

    Add redux actions

---
+++ actions/index.js
@@ -0,0 +1,16 @@
+export const RECEIVE_ENTRIES = "RECEIVE_ENTRIES";
+export const ADD_ENTRY = 'ADD_ENTRY'
+
+export const receiveEntries = entries => (
+  {
+    type: RECEIVE_ENTRIES,
+    entries
+  }
+)
+
+export const addEntry = entry => (
+  {
+    type: addEntry,
+    entry
+  }
+)

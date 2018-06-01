87e14d8268e1f67e74ffef542e5958000a0d16df
Xavier Arques <xavierarques@yahoo.com>
2018-06-01 14:58:34 +0200

    Add Slider

--- components/UdaciSlider.js
+++ components/UdaciSlider.js
@@ -1,10 +1,20 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { Text, View, Slider } from 'react-native';
 
-const UdaciSlider = () => {
+const UdaciSlider = ({ max, unit, step, value, onChange }) => {
   return (
     <View>
-      <Text>UdaciSlider</Text>
+      <Slider
+        step={step}
+        value={value}
+        maximumValue={max}
+        minimumValue={0}
+        onValueChange={onChange}
+      />
+      <View>
+        <Text>{value}</Text>
+        <Text>{unit}</Text>
+      </View>
     </View>
   );
 };

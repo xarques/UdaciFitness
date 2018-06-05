9dd812d2b54211c4dbe6cd103199b2809b57755b
Xavier Arques <xavierarques@yahoo.com>
2018-06-05 22:27:39 +0200

    Manage granted status

--- components/Live.js
+++ components/Live.js
@@ -2,16 +2,51 @@ import React, { Component } from "react";
 import { View, Text, ActivityIndicator, TouchableOpacity, StyleSheet } from "react-native";
 import { Foundation } from '@expo/vector-icons';
 import { purple, white } from '../utils/colors';
+import { Location, Permissions } from "expo";
+import { calculateDirection } from "../utils/helpers";
 
 export default class Live extends Component {
   state = {
     coords: null,
-    status: "denied",
+    status: "granted",
     direction: ""
   };
 
+  componentDidMount = () => {
+    Permissions.getAsync(Permissions.LOCATION)
+      .then(({ status }) => {
+        if (status === 'granted') {
+          return this.setLocation()
+        }
+
+        this.setStatus(() => ({ status }))
+      })
+      .catch((error) => {
+        console.warn('Error getting Location permission: ', error);
+
+        this.setState(() => ({ status: 'undetermined' }))
+      })
+  }
+
   askPermission = () => {
 
+  }
+
+  setLocation = () => {
+    Location.watchPositionAsync({
+      enableHighAccuracy: true,
+      timeInterval: 1,
+      distanceInterval: 1
+    }, ({ coords }) => {
+      const newDirection = calculateDirection(coords.heading);
+      const { direction } = this.state;
+
+      this.setState(() => ({
+        coords,
+        status: 'granted',
+        direction: newDirection
+      }))
+    })
   }
 
   render() {
@@ -48,9 +83,31 @@ export default class Live extends Component {
     }
 
     return (
-      <View>
-        <Text>Live</Text>
-        <Text>{JSON.stringify(this.state)}</Text>
+      <View style={styles.container}>
+        <View style={styles.directionContainer}>
+          <Text style={styles.header}>You're heading</Text>
+          <Text style={styles.direction}>
+            North
+          </Text>
+        </View>
+        <View style={styles.metricContainer}>
+          <View style={styles.metric}>
+            <Text style={[styles.header, { color: white }]}>
+              Altitude
+            </Text>
+            <Text style={[styles.subHeader, { color: white }]}>
+              {200} feet
+            </Text>
+          </View>
+          <View style={styles.metric}>
+            <Text style={[styles.header, { color: white }]}>
+              Speed
+            </Text>
+            <Text style={[styles.subHeader, { color: white }]}>
+              {300} MPH
+            </Text>
+          </View>
+        </View>
       </View>
     )
   }
@@ -59,24 +116,57 @@ export default class Live extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'space-between'
+    justifyContent: "space-between"
   },
   center: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
+    justifyContent: "center",
+    alignItems: "center",
     marginLeft: 30,
-    marginRight: 30,
+    marginRight: 30
   },
   button: {
     padding: 10,
     backgroundColor: purple,
-    alignSelf: 'center',
+    alignSelf: "center",
     borderRadius: 5,
-    margin: 20,
+    margin: 20
   },
   buttonText: {
     color: white,
-    fontSize: 20,
+    fontSize: 20
+  },
+  directionContainer: {
+    flex: 1,
+    justifyContent: "center"
+  },
+  header: {
+    fontSize: 35,
+    textAlign: "center"
+  },
+  direction: {
+    color: purple,
+    fontSize: 120,
+    textAlign: "center"
+  },
+  metricContainer: {
+    flexDirection: "row",
+    justifyContent: "space-around",
+    backgroundColor: purple
+  },
+  metric: {
+    flex: 1,
+    paddingTop: 15,
+    paddingBottom: 15,
+    backgroundColor: "rgba(255, 255, 255, 0.1)",
+    marginTop: 20,
+    marginBottom: 20,
+    marginLeft: 10,
+    marginRight: 10
+  },
+  subHeader: {
+    fontSize: 25,
+    textAlign: "center",
+    marginTop: 5
   }
-})
+});
